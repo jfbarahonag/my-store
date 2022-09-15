9abc351dfc3735594c3064e716feed9c924ccb63
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2022-09-15 08:00:02 -0500

    new endpoints were added for testing JSON objects

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const host = 'localhost'
+// const host = 'localhost'
+const host = '172.25.111.204'
+
+let products = [
+  {
+    name:"Product 1",
+    price:"1.00",
+    stock:"1",
+  },
+  {
+    name:"Product 2",
+    price:"2.00",
+    stock:"2",
+  },
+]
+
+let categories = [
+  {
+    name:"Category 1",
+  },
+  {
+    name:"Category 2",
+  },
+]
 
 app.get('/', (req, res) => {
   res.send('Hi from Express');
+});
+
+app.get('/products', (req, res) => {
+  res.json(products)
+});
+
+app.get('/categories', (req, res) => {
+  res.json(categories)
 });
 
 app.listen(port, host, () => {
