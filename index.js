6b0e0a924e5bec6465822c35bda8ba9b5a7606df
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2022-09-14 21:13:25 -0500

    first route was added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('my app');
+const express = require('express');
+const app = express();
+const port = 3000;
+const host = 'localhost'
+
+app.get('/', (req, res) => {
+  res.send('Hi from Express');
+});
+
+app.listen(port, host, () => {
+  console.log(`Listening on port ${port}`);
+});
