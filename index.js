c8496cf85cc6d5b39524716455f1a75e128007b2
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2022-09-15 09:09:06 -0500

    categories enspoints were added and detail endpoint for products was added

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// const host = 'localhost'
-const host = '172.25.111.204'
+const host = 'localhost'
 
 let products = [
   {
@@ -20,9 +19,11 @@ let products = [
 let categories = [
   {
     name:"Category 1",
+    products:[]
   },
   {
     name:"Category 2",
+    products:[]
   },
 ]
 
@@ -34,9 +35,26 @@ app.get('/products', (req, res) => {
   res.json(products)
 });
 
+app.get('/products/:product_id', (req, res) => {
+  const { product_id } = req.params
+  const product = (product_id > 0 && product_id <=products.length) ? {id: product_id, ...products.at(product_id-1)}:{}
+  res.json(product)
+})
+
 app.get('/categories', (req, res) => {
   res.json(categories)
 });
+
+app.get('/categories/:category_id', (req, res) => {
+  const { category_id } = req.params
+  const category = (category_id > 0 && category_id <=categories.length) ? {id: category_id, ...categories.at(category_id-1)}:{}
+  res.json(category)
+})
+
+app.get('/categories/:category_id/products/:product_id', (req, res) => {
+  const {category_id, product_id} = req.params
+  res.json({category_id, product_id})
+})
 
 app.listen(port, host, () => {
   console.log(`Listening on port ${port}`);
