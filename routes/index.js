b16f4ff5c423778d4c78f38714ba83546bb1d78d
Juan Felipe Barahona Gonzalez <jbarahona@XM.com.co>
2022-09-16 13:42:41 -0500

    new route was added to be able to add versions to the API

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
+const express = require('express')
 const categories_router = require('./categories.router')
 const products_router = require('./products.router')
 const users_router = require('./users.router')
 
 function router_api(app) {
-    app.use('/categories', categories_router)
-    app.use('/products', products_router)
-    app.use('/users', users_router)
+    const router = express.Router()
+    app.use('/api/v1', router)
+    
+    router.use('/categories', categories_router)
+    router.use('/products', products_router)
+    router.use('/users', users_router)
 }
 
 module.exports = router_api
