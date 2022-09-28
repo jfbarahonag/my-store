330fc79f5ad3033de3043196be774309b78a9a9c
Juan Felipe Barahona Gonzalez <jbarahona@XM.com.co>
2022-09-28 15:00:54 -0500

    code was changed to use environment variables

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,12 +1,14 @@
 const { Client } = require('pg')
 
+const { DB_USER, DB_NAME, DB_PASSWORD } = process.env
+
 const getConnection = async () => {
     const client = new Client({
         host: 'localhost',
         port: 5432,
-        user: 'felipe',
-        password:'admin123',
-        database:'jfbg-store'
+        user: DB_USER,
+        password:DB_PASSWORD,
+        database:DB_NAME
     })
     await client.connect();
 
